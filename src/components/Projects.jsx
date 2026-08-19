96a4d07ee8c317fe70fbf2c3d9ea58f194b2f2e5
Sumbal Saeed <sumbalsaeed125@gmail.com>
2026-08-20 01:08:50 +0500

    Add CurrencyConverter component to Projects.jsx

---
+++ src/components/Projects.jsx
@@ -0,0 +1,20 @@
+function CurrencyConverter () {
+    return (
+        <>
+         <div className="font-montserrat">
+        <h1 className="flex justify-center text-white pt-8 font-bold text-[32px] text-balance tracking-widest">
+          {" "}
+          OUR
+          <span className="px-2 bg-clip-text text-transparent bg-linear-to-r from-cyan-100 to-sky-300">
+            PROJECTS
+          </span>
+        </h1>
+        <div className="flex items-center justify-center">
+        <img src="src/assets/projects.png" className="w-200 h-200 pt-10" />
+        </div>
+      </div>
+        </>
+    );
+}
+
+export default CurrencyConverter;
