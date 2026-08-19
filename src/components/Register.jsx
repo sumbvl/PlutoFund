d3b18119072920c4848bc75d4dba014496b5a59c
Sumbal Saeed <sumbalsaeed125@gmail.com>
2026-08-20 01:07:18 +0500

    Implement registration form with password confirmation

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Register() {
+
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    function handleRegister(e) { 
+    if(password!==confirm) {
+        alert("Passwords do not match.");
+        return;
+    }
+    alert("Registered successfully!");
+    navigate("/login");
+   }
   return (
     <>
     <div className="relative min-h-screen overflow-hidden">
@@ -10,7 +25,7 @@ function Register() {
      </div>
       <div className="relative z-10 max-w-lg max-h-screen px-6 mb-5 mx-auto mt-15 bg-cyan-950 rounded-[20px] font-montserrat text-white border border-cyan-400/40 shadow-2xl shadow-cyan-500/25">
         <h1 className="text-center py-6 mt-3 text-[30px] font-bold bg-clip-text text-transparent bg-linear-to-r from-white to-cyan-300">Register</h1>
-        <form className="flex flex-col gap-4 text-base px-8">
+        <form onSubmit={handleRegister} className="flex flex-col gap-4 text-base px-8">
           <div className="flex flex-col">
             <label className="text-sm mb-1 pl-3">Full Name</label>
             <input
@@ -36,9 +51,9 @@ function Register() {
               className="focus:outline-none focus:border-cyan-300 focus:ring-2 focus:ring-cyan-400/40 shadow-md shadow-cyan-500/10 transition rounded-[30px] py-2 px-4 bg-slate-900/80 border border-cyan-500/30 focus:outline-none focus:ring-2 focus:ring-cyan-300" />
           </div>
           <div className="flex justify-center pt-2 text-cyan-950 mt-2">
-            <Link className="text-cyan-900 px-20 rounded-[30px] py-2.5 bg-gradient-to-r from-sky-200 to-cyan-300 font-semibold hover:opacity-90 transition">
+            <button type="submit" className="text-cyan-900 px-20 rounded-[30px] py-2.5 bg-gradient-to-r from-sky-200 to-cyan-300 font-semibold hover:opacity-90 transition">
               Create Account
-            </Link>
+            </button>
           </div>
           <p className="text-xs pb-6 text-cyan-200/70 flex items-center justify-center">
               Already have an account? <Link to="/login" className="pl-2 text-cyan-300 hover:underline">Log in</Link>
