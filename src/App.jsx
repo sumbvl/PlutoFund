469438d0a087513a35826474761021670dd58e68
Sumbal Saeed <sumbalsaeed125@gmail.com>
2026-08-19 20:01:32 +0500

    Refactor App component and add routes for Login/Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { Route, Routes } from 'react-router-dom'
-import NavBar from './components/NavBar'
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import Body from './components/Body';
+import Login from './components/Login';
+import Register from './components/Register';
 
-export default function App () {
+export default function App() {
   return (
-    <div className='bg-radial-[at_99%_45%] from-sky-700 via-sky-950 to-slate-950 to-90%'>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Body />} />
-    </Routes>
+    <div className="min-h-screen w-full bg-radial-[at_99%_45%] from-sky-700 via-sky-950 to-slate-950 to-90%">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
